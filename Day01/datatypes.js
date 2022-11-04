d3b9592f061e30776508b379dbb08378a586f80c
Dubem <dubemmegbo@gmail.com>
2022-11-04 19:44:28 +0100

    Updated Day01's assignment

---
+++ Day01/datatypes.js
@@ -0,0 +1,27 @@
+let height = 30
+let name = `Dubem`
+
+console.log(typeof(age))
+console.log(typeof(name))
+
+// let coutry 
+// let boy
+// let isHappy
+// let nice 
+
+// let home = lagos
+// let gender = boy
+// let isPaid = true
+// let isAngry = false
+
+// let firstName = "Dubem"
+// let lastName = "Egbo"
+// let maritalStatus = "Single"
+// let country = 'Nigeria'
+// //let age = 20
+
+let myAge = 25
+let yourAge = 30
+
+console.log('I am ' + myAge + ' years old.')
+console.log('You are ' + yourAge + ' years old.')
---
+++ Day01/variables.js
@@ -0,0 +1,4 @@
+let word = `JavaScript`
+let isValid = true
+let age = undefined
+let head = null
